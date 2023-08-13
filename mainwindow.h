3737df9b610e5720bcc15912cc9d258eec0759bf
Amina1412 <amina_b@outlook.de>
2023-08-13 17:28:48 +0200

    + added light mode + preview

--- mainwindow.h
+++ mainwindow.h
@@ -29,19 +29,37 @@ private slots:
 
 //    void on_DisplayComboBox_activated(int index);
 
-//    void on_previewToolButton_triggered(QAction *arg1);
 
     void on_pathLineEdit_editingFinished();
 
     void on_DisplayComboBox_currentIndexChanged(int index);
 
+    void on_listWidget_currentItemChanged(QListWidgetItem *current, QListWidgetItem *previous);
+
+    void on_previewToolButton_clicked();
+
+    void on_listWidget_itemClicked(QListWidgetItem *item);
+
+    void on_darkModePushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    QString m_darkstyle;
+    QString m_lightstyle;
+
     QFileSystemModel *m_dirmodel;
     QFileSystemModel *m_filemodel;
+
+    QString m_currentDir;
+    QListWidgetItem m_currentFile;
+
     QListWidget *listWidget;
     QTreeView *treeView;
     QLineEdit *searchLineEdit;
     QPushButton *refreshButton;
+    bool m_preview_on;
+    bool m_lightmode_on;
+    void updateListWidget(QString sPath);
 };
 #endif // MAINWINDOW_H
