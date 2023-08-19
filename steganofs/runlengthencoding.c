c779d35779088e20d4938a79bbc25dc6bcfa93ad
D-Dennis <64061670+D-Dennis@users.noreply.github.com>
2023-08-19 16:21:30 +0200

    started runlengthencoding implementation

---
+++ steganofs/runlengthencoding.c
@@ -0,0 +1,5 @@
+#include "runtimelength.h"
+
+void runtimelength(struct SerializedFilesystem serializedFilesystem) {
+    if (serializedFilesystem)
+}
---
+++ steganofs/runlengthencoding.h
@@ -0,0 +1,8 @@
+#ifndef STEGANOFS_MAIN_RUNTIMELENGTH_H
+#define STEGANOFS_MAIN_RUNTIMELENGTH_H
+
+#include "ramdiskloader.h"
+
+size_t runtimelength(struct SerializedFilesystem serializedFilesystem);
+
+#endif //STEGANOFS_MAIN_RUNTIMELENGTH_H
